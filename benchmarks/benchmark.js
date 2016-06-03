337632772bd496c1a710b714af9bb874f6b72cd8
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2016-06-03 17:16:46 +0430

    bench marks added

---
+++ benchmarks/benchmark.js
@@ -0,0 +1,32 @@
+'use strict';
+var logeek = require('../dist/logeek').logeek;
+
+suite('logeek', function () {
+	set('iterations', 100000);
+
+  logeek.show('nothing');
+
+  bench('simple log', function () {
+		logeek('Hi');
+	});
+
+  bench('array input log', function () {
+    logeek(['one', 'tow', 'three']);
+	});
+
+  bench('scope defined log', function () {
+    logeek('Hi @ S');
+	});
+
+  bench('nested scope log', function () {
+    logeek('Hi @ S/M/G');
+	});
+
+  logeek.show('*/M/F');
+  bench('reverse scope half matched', function () {
+    logeek('Hi @ S/M/G');
+	});
+  logeek.show('nothing');
+
+
+});
