29eda008241630b47403a8f6e05edbebac9d435f
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2016-06-03 10:20:03 +0430

    minified version added

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,26 @@ var gulp = require('gulp'),
     sourcemaps = require('gulp-sourcemaps'),
     concat = require('gulp-concat'),
     babel = require('gulp-babel'),
-    watch = require('gulp-watch');
+    watch = require('gulp-watch'),
+    rename = require('gulp-rename'),
+		uglify = require('gulp-uglify');
 
-gulp.task('default', function () {
+gulp.task('ES6', function () {
   return gulp.src('src/logeek.js').
     pipe(sourcemaps.init()).
     pipe(babel({modules: 'ignore'})).
     pipe(sourcemaps.write('.')).
     pipe(gulp.dest('dist'));
 });
+
+gulp.task('uglify', function () {
+  return gulp.src('dist/logeek.js').
+    pipe(uglify()).
+    pipe(rename('logeek.min.js')).
+    pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['ES6', 'uglify']);
 
 gulp.task('watch', function () {
   watch('src/**', function () {
