554e49804f858228248450f734377e32029e2a20
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2016-06-10 14:04:39 +0430

    async tests added

--- test/main.test.js
+++ test/main.test.js
@@ -122,113 +122,47 @@ describe('Scoped logs', function () {
     logeek('a').at(' x  ');
     expect(console.log.firstCall.calledWith('a')).to.be.true;
     
+    console.log.restore();
   });
 });
 
-// describe('Scopped loggs', function () {
-//   it('should\'t log the messages in other scopes', function () {
-//     sinon.spy(console, 'log');
-// 
-//     logeek.show('nothing');
-//     logeek('msg1');
-//     logeek('nothing');
-//     logeek(['m', 's', 'g', '1']);
-//     logeek.show('_global_');
-// 
-//     expect(console.log.callCount).to.equal(0);
-// 
-//     console.log.restore();
-//   });
-// 
-//   it('should log the messages in their scope', function () {
-//     sinon.spy(console, 'log');
-// 
-//     logeek.show('myScope');
-//     logeek('msg1 @ myScope');
-//     logeek('msg2 @ hisScope');
-// 
-//     expect(console.log.callCount).to.equal(1);
-//     expect(console.log.calledWith('msg1')).to.be.true;
-// 
-//     logeek.show('_global_');
-//     console.log.restore();
-//   });
-// 
-//   it('should detect nested scopes', function () {
-//     sinon.spy(console, 'log');
-//     
-//     logeek.show('myScope/*');
-//     logeek('msg1 @ myScope');
-//     logeek('msg2 @ myScope/subScope');
-//     logeek('msg3 @ myScope/a/b/c/d');
-//     logeek('msg4 @ X/myScope/subScope/X');
-//     logeek('msg5 @ myScope//');
-//     
-//     expect(console.log.callCount).to.equal(3);
-//     expect(console.log.calledWith('msg1')).to.be.true;
-//     expect(console.log.calledWith('msg2')).to.be.true;
-//     expect(console.log.calledWith('msg3')).to.be.true;
-//     
-//     logeek.show('_global_');
-//     console.log.restore();
-//   });
-//   
-//   it('should detect reverse filter expressions', function () {
-//     sinon.spy(console, 'log');
-// 
-//     logeek.show('*/myScope');
-//     logeek('msg1 @ myScope');
-//     logeek('msg2 @ parent/myScope');
-//     logeek('msg3 @ a/b/c/d/myScope');
-//     logeek('msg4 @ X/myScope/subScope/X');
-//     logeek('msg5 @ myScope//');
-//     
-//     expect(console.log.callCount).to.equal(3);
-//     expect(console.log.calledWith('msg1')).to.be.true;
-//     expect(console.log.calledWith('msg2')).to.be.true;
-//     expect(console.log.calledWith('msg3')).to.be.true;
-//     
-//     logeek.show('_global_');
-//     console.log.restore();
-//   });
-// });
-// 
-// describe('async calls', function () {
-//   
-//   it('should log when called async', function (done) {
-//     sinon.spy(console, 'log');
-//     
-//     logeek.show('myScope');
-//     
-//     setTimeout(function () {
-//       logeek('a-msg1 @ myScope');
-// 
-//       expect(console.log.calledWith('a-msg1')).to.be.true;
-//       done();
-// 
-//       logeek.show('_global_');
-//       console.log.restore();
-//     }, 1);
-//       
-//   });
-//   
-//   it('should be able to work when called indirectly', function (done) {
-//     sinon.spy(console, 'log');
-//     
-//     logeek.show('myScope');
-//     var sampleFunc = function (msg, done) {
-//       setTimeout(function () {
-//         logeek(msg + ' @ myScope');
-//         
-//         expect(console.log.calledWith('a-msg1')).to.be.true;
-//         done();
-//         
-//         logeek.show('_global_');
-//         console.log.restore();
-//       }, 1);
-//     };
-//     
-//     sampleFunc('a-msg1', done);
-//   });
-//   
-// });
+describe('async calls', function () {
+  
+  it('should log when called async', function (done) {
+    setup();
+    
+    logeek.show('s');
+    
+    setTimeout(function () {
+      logeek('a @ s');
+      logeek('b').at('s');
+
+      expect(console.log.firstCall.calledWith('a ')).to.be.true;
+      expect(console.log.secondCall.calledWith('b')).to.be.true;
+      done();
+
+      console.log.restore();
+    }, 1);
+      
+  });
+  
+  it('should be able to work when called indirectly', function (done) {
+    setup();
+    
+    logeek.show('s');
+    var sampleFunc = function (msg, done) {
+      setTimeout(function () {
+        logeek(msg + ' @ s');
+        
+        expect(console.log.firstCall.calledWith('a ')).to.be.true;
+        done();
+        
+        logeek.show('_global_');
+        console.log.restore();
+      }, 1);
+    };
+    
+    sampleFunc('a', done);
+  });
+  
+});
